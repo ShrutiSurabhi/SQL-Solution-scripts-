3d057959c280f2ab4e91e445ebaae9afa5db0451
ShrutiSurabhi <80778530+ShrutiSurabhi@users.noreply.github.com>
2021-07-05 08:36:10 -0500

    Update sql_q2_sol.sql

--- sql_q2_sol.sql
+++ sql_q2_sol.sql
@@ -1,3 +1,4 @@
+//******2. Write a SQL query to find the number of matches that were won by penalty shootout.*******//
 SELECT
 	COUNT(CASE WHEN decided_by = 'P' THEN 1 END)
 		FROM [euro_cup_2016].[dbo].[match_details]; 
