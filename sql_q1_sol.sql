4ceb00da0d590ed1d58dd501fa82c955e1f5301d
ShrutiSurabhi <80778530+ShrutiSurabhi@users.noreply.github.com>
2021-07-05 08:35:21 -0500

    Update sql_q1_sol.sql

--- sql_q1_sol.sql
+++ sql_q1_sol.sql
@@ -1,4 +1,4 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
+/****** 1. Write a SQL query to find the date EURO Cup 2016 started on.  ******/
 
 SELECT min(play_date)
 FROM [euro_cup_2016].[dbo].[match_mast]
