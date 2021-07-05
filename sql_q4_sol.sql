5d53eb865b1f0a0a10e774469cb1b46b6d2b1fd2
ShrutiSurabhi <80778530+ShrutiSurabhi@users.noreply.github.com>
2021-07-05 08:37:43 -0500

    Update sql_q4_sol.sql

--- sql_q4_sol.sql
+++ sql_q4_sol.sql
@@ -1,3 +1,7 @@
+/*****4. Write a SQL query to compute a list showing the number of substitutions that
+happened in various stages of play for the entire tournament.******/
+
+
 SELECT play_half,play_schedule,COUNT(*) 
 FROM player_in_out 
 WHERE in_out='I'
