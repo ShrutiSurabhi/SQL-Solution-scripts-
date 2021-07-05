37e4b8c1a38e9a52b52b1a0edf448f0f47981dfd
ShrutiSurabhi <80778530+ShrutiSurabhi@users.noreply.github.com>
2021-07-05 08:36:56 -0500

    Update sql_q3_sol.sql

--- sql_q3_sol.sql
+++ sql_q3_sol.sql
@@ -1,4 +1,6 @@
-
+/****** 3. Write a SQL query to find the match number, date, and score for matches in which no
+stoppage time was added in the 1st half.
+  ******/
 
 SELECT match_no, play_date, goal_score 
 FROM [euro_cup_2016].[dbo].[match_mast]
