47ff1a86fac0b49021d917107d64137ce8817d1f
ShrutiSurabhi <80778530+ShrutiSurabhi@users.noreply.github.com>
2021-07-05 08:44:55 -0500

    Update sql_q4_sol.sql

--- sql_q4_sol.sql
+++ sql_q4_sol.sql
@@ -2,8 +2,7 @@
 happened in various stages of play for the entire tournament.******/
 
 
-SELECT play_half,play_schedule,COUNT(*) 
-FROM player_in_out 
-WHERE in_out='I'
-GROUP BY play_half,play_schedule
-ORDER BY play_half,play_schedule,count(*);
+SELECT play_half,play_schedule,COUNT(*) FROM player_in_out 
+       WHERE in_out='I'
+       GROUP BY play_half,play_schedule
+       ORDER BY play_half,play_schedule,count(*);
